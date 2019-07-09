447d02045e45bff36fc8dc13311c652e4c2d63fc
Mandeep Singh <mandeep.singh671@gmail.com>
2019-07-09 14:03:02 +0530

    code commit

---
+++ CoreLocationDemo/CurrentLocationSingleton/FetchLocationSingleton.swift
@@ -0,0 +1,105 @@
+//
+//  File.swift
+//  Sugo
+//
+//  Created by Mandeep Singh on 24/04/18.
+//  Copyright © 2018 Mandeep Singh. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import UIKit
+
+struct CompletionBlocks {
+    
+    typealias closure = (_ returnObject: AnyObject?, _ locationEnabled: AnyObject?) -> ()
+}
+
+class FetchLocation : NSObject {
+    
+    static let SharedManager = FetchLocation()
+    
+    //Step - 1: Initialize ClLocation Mananger
+    var locationManager : CLLocationManager!
+    
+    //Step - 2: Declare Variables
+    private var userCurrentLocation = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var completionBlock : CompletionBlocks.closure?
+    
+    //Step - 3: Create functions for Location Manager
+    private override init() {
+        super.init()
+
+        locationManager = CLLocationManager()
+    }
+    
+    func startMonitoring() {
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.delegate = self
+            self.startUpdatingLocation()
+        } else {
+            guard let cB = self.completionBlock else { return }
+            cB(nil, false as AnyObject)
+        }
+    }
+    
+    private func startUpdatingLocation() {
+        DispatchQueue.main.async(execute: {
+            self.locationManager.startUpdatingLocation()
+        })
+    }
+    
+    private func stopUpdatingLocation() {
+        DispatchQueue.main.async(execute: {
+            self.locationManager.stopUpdatingLocation()
+        })
+    }
+}
+
+extension FetchLocation : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print(error.localizedDescription)
+        guard let cB = self.completionBlock else { return }
+        cB(nil, error as AnyObject)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            
+            let newCoordinate = manager.location!.coordinate
+            
+            if userCurrentLocation.latitude != newCoordinate.latitude && userCurrentLocation.longitude != newCoordinate.longitude {
+                userCurrentLocation = manager.location!.coordinate
+                guard let cB = self.completionBlock else { return }
+                cB(userCurrentLocation as AnyObject, nil)
+            }
+            
+            self.stopUpdatingLocation()
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .denied:
+            guard let cB = self.completionBlock else { return }
+            cB(nil, false as AnyObject)
+        case .authorizedWhenInUse:
+            self.startUpdatingLocation()
+        case .notDetermined:
+            self.startUpdatingLocation()
+        default:
+            self.startUpdatingLocation()
+        }
+    }
+    
+}
---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Mandeep Singh on 24/04/18.
+//  Copyright © 2018 Mandeep Singh. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    var userCoords = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Fetch User Current Location
+        self.fetchUserCurrentLocation()
+    }
+    
+    func fetchUserCurrentLocation() {
+        
+        let locationFetch = FetchLocation.SharedManager
+        locationFetch.startMonitoring()
+        locationFetch.completionBlock = { [weak self] (userCoordinates, errorBlock) in
+            
+            guard let self = self else { return }
+            
+            if let isLocationDisabled = errorBlock as? Bool, isLocationDisabled == false {
+                print("User Location is Disabled")
+                self.showPopIfLocationServiceIsDisable()
+                return
+            }
+            
+            if let locationError = errorBlock as? Error {
+                print(locationError.localizedDescription)
+            }
+            
+            if let userLocation = userCoordinates as? CLLocationCoordinate2D {
+                print(userLocation.latitude, userLocation.longitude)
+                self.userCoords = userLocation
+            }
+        }
+    }
+    
+    private func showPopIfLocationServiceIsDisable() {
+        
+        let alert = UIAlertController(title: "Access to GPS is restricted", message: "GPS access is restricted. Draw path by location, please enable GPS in the Settings > Privacy > Location Services.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Go to Settings now", style: UIAlertActionStyle.default, handler: { (alert: UIAlertAction!) in
+            UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!, options: [:], completionHandler: nil)
+        }))
+        self.present(alert, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
